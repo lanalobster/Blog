2ae46a7cb5be98f3f5d96a3e3583dbbfdf873ad7
Roksolana Hudyma <lana.gudyma15@gmail.com>
2020-05-29 15:13:55 +0300

    Users added

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,6 @@ class Article < ApplicationRecord
     length: { minimum: 3, maximum: 30 }
     validates :text, presence: true,
     length: { minimum: 3, maximum: 1000 }
+    belongs_to :user
+    validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_142633) do
+ActiveRecord::Schema.define(version: 2020_05_29_112332) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -26,6 +27,13 @@ ActiveRecord::Schema.define(version: 2020_05_09_142633) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "articles"
