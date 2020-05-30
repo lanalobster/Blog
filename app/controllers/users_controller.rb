6044bec5e9ea6c5cb50e777c4f02c2bb5ff8214c
Roksolana Hudyma <lana.gudyma15@gmail.com>
2020-05-31 01:14:08 +0300

    Users page looking good now. Password check added(confirmation).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
     def index
-        @users = User.paginate(page: params[:page], per_page: 5)
+        @users = User.paginate(page: params[:page], per_page: 20)
     end
 
     def show
         @user = User.find(params[:id])
-        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+        @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
     end
 
     def new
@@ -18,21 +18,31 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save
-         flash[:success] = "Welcome to MyBlog! #{@user.username}"
-         redirect_to articles_path
+        if user_params[:password] != user_params[:repeat_password]
+            flash[:notice] = "Passwords don't match!"
+            render 'edit'
         else
-         render 'new'
+            if @user.save
+                flash[:success] = "Welcome to MyBlog! #{@user.username}"
+                redirect_to articles_path
+            else
+                render 'new'
+            end
         end
     end  
     
     def update
         @user = User.find(params[:id])
-        if @user.update(user_params)
-            flash[:success] = "Your account was updated successfully"
-            redirect_to articles_path
+        if user_params[:password] != user_params[:repeat_password]
+            flash[:notice] = "Passwords don't match!"
+            render 'edit'
         else
-             render 'edit'
+            if @user.update(user_params)
+                flash[:success] = "Your account was updated successfully"
+                redirect_to articles_path
+            else
+                render 'edit'
+            end
         end
     end
 
@@ -40,10 +50,11 @@ class UsersController < ApplicationController
         max_id = Username.maximum(:id)
         random_id = rand(1..max_id)
         username = Username.find(random_id)
+        @username = username.adjective
         random_id = rand(1..max_id)
         username = Username.find(random_id)
         time = Time.now
-        @username = username.adjective + "_"+ username.noun + "#{time.sec}"
+        @username += "_"+ username.noun + "#{time.sec}"
         respond_to do |format|
            format.js
         end
@@ -51,7 +62,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:username, :email, :password, :repeat_password)
     end
 
 end
